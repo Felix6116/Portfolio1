f3b4b78eae80d34048f93e6f4ed7d1770c62c836
Felix6116 <felix.despois@sts-sio-caen.info>
2023-12-23 15:07:39 +0100

    update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     var header = document.getElementById("myHeader");
     var lastScrollTop = 0;
-    var openMenuButton = document.getElementById("openmenu"); // Ajout de la déclaration
+    var openMenuButton = document.getElementById("openmenu"); 
 
     window.addEventListener("scroll", function () {
         var currentScrollTop = window.scrollY;
 
-        // Ajoutez ou supprimez la classe "sticky" en fonction du défilement et du sens
+ 
         if (currentScrollTop > lastScrollTop) {
             header.classList.add("sticky");
         } else {
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function () {
         lastScrollTop = currentScrollTop;
     });
 
-    // Event listener to remove the sticky class when the button is clicked
     openMenuButton.addEventListener('click', function() {
         header.classList.remove('sticky');
         page.classList.add('menuopen');
@@ -26,20 +25,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     links.forEach(function(link) {
         link.addEventListener('click', function(event) {
-            // Prevent the default action
+           
             event.preventDefault();
 
-            // Get the target element
+           
             var targetId = this.getAttribute('href');
             var targetElement = document.querySelector(targetId);
 
-            // Smooth scroll to target with a slight delay
+          
             if (targetElement) {
                 setTimeout(function() {
                     targetElement.scrollIntoView({
                         behavior: 'smooth'
                     });
-                }, 100); // Change the delay time (in milliseconds) as needed
+                }, 100); 
             }
         });
     });
@@ -55,16 +54,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
         scrollDownArrows.forEach(function (arrow) {
             arrow.addEventListener('click', function () {
-                // Spécifiez l'ID de la section vers laquelle vous voulez faire défiler la page
+                
                 var targetSectionId = this.getAttribute('data-target');
         
-                // Affichez l'ID de la section cible dans la console pour déboguer
+                
                 console.log("Arrow clicked! Target section ID:", targetSectionId);
         
-                // Récupérer l'élément de la section cible
+                
                 var targetSection = document.getElementById(targetSectionId);
         
-                // Vérifier si la section cible existe
+                
                 if (targetSection) {
                     console.log("Target section found:", targetSection);
                     setTimeout(function () {
